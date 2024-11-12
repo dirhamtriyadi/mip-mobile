5a22929f039e57c727c4443428ed014b50114af0
dirhamtriyadi <dirhamtriyadi@gmail.com>
2024-11-12 14:55:42 +0700

    style: remove field label and input style for izin page
    Remove unused field label and input style that are no longer needed.

--- src/screens/Izin/index.tsx
+++ src/screens/Izin/index.tsx
@@ -238,18 +238,4 @@ const styles = StyleSheet.create({
   groupField: {
     width: '100%',
   },
-  fieldLabel: {
-    fontSize: 16,
-    marginBottom: 10,
-    fontWeight: 'bold',
-  },
-  fieldInput: {
-    width: '100%',
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    marginBottom: 15,
-    paddingHorizontal: 10,
-  },
 });
