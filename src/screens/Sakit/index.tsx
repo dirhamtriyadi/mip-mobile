39f8172e24f3ea49ea30edbb80b6afe5de2cc4f7
dirhamtriyadi <dirhamtriyadi@gmail.com>
2024-11-12 14:55:20 +0700

    feat(sakit): implement field hooks and custom components
    Implement various field hooks and reusable components for field inputs, date and time pickers, image pickers, and location pickers.
    
    By moving form validation logic to the hook level, we improve code reusability and streamline the form management process. These custom components further enhance the usability and maintainability of our application.

--- src/screens/Sakit/index.tsx
+++ src/screens/Sakit/index.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, PermissionsAndroid, Platform, Alert, Image } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, Alert, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DatePicker from 'react-native-date-picker';
 import MapView, { Marker } from 'react-native-maps';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import { launchCamera } from 'react-native-image-picker';
 import { useCurrentLocation } from '../../hooks/useCurrentLocation';
 import instance from "../../configs/axios";
 import { useUserData } from "../../hooks/useUserData";
@@ -12,18 +11,15 @@ import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../../../App";
 import dayjs from "dayjs";
 import { useNotification } from "../../hooks/useNotification";
+import useCamera from "../../hooks/useCamera";
+import useDatePickerStartDate from "../../hooks/useDatePicker";
+import useDatePickerEndDate from "../../hooks/useDatePicker";
+import useTimePicker from "../../hooks/useTimePicker";
+import InputField from "../../components/InputField";
+import ImagePicker from "../../components/ImagePicker";
+import LocationPicker from "../../components/LocationPicker";
 
-function SakitScreen() {
-  const [image, setImage] = useState<any>(null);
-  const [openDatePickerStartDate, setOpenDatePickerStartDate] = useState(false);
-  const [openDatePickerEndDate, setOpenDatePickerEndDate] = useState(false);
-  const [openTimePicker, setOpenTimePicker] = useState(false);
-  const { userDetailData } = useUserData();
-  const { location, getCurrentLocation } = useCurrentLocation();
-  const { showNotification } = useNotification();
-
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
-
+function SakitScreen() {  
   const [data, setData] = useState({
     code: '',
     nik: '',
@@ -37,6 +33,16 @@ function SakitScreen() {
     latitude: 0,
     longitude: 0,
   });
+
+  const { image, handleClickOpenCamera, handleClickResetCamera } = useCamera();
+  const { date: startDate, openDatePicker: openDatePickerStartDate, setOpenDatePicker: setOpenDatePickerStartDate, handleDateChange: handleDateChangeStartDate } = useDatePickerStartDate(data.start_date);
+  const { date: endDate, openDatePicker: openDatePickerEndDate, setOpenDatePicker: setOpenDatePickerEndDate, handleDateChange: handleDateChangeEndDate } = useDatePickerEndDate(data.end_date);
+  const { time, openTimePicker, setOpenTimePicker, handleTimeChange } = useTimePicker(data.time_check_in);
+  const { userDetailData } = useUserData();
+  const { location, getCurrentLocation } = useCurrentLocation();
+  const { showNotification } = useNotification();
+
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   useEffect(() => {
     if (location.latitude !== 0 && location.longitude !== 0) {
@@ -55,26 +61,12 @@ function SakitScreen() {
       code: userDetailData.name + data.start_date.format('DD/MM/YYYY'),
       nik: userDetailData.nik,
       name: userDetailData.name,
+      start_date: startDate,
+      end_date: endDate,
+      time_check_in: time,
+      image_check_in: image,
     }));
-  }, [userDetailData, data.start_date]);
-
-  const handleDateChangeStartDate = (selectedDate: Date) => {
-    // setData((prevData) => ({ ...prevData, date: selectedDate }));
-    setData((prevData) => ({ ...prevData, start_date: dayjs(selectedDate) }));
-    setOpenDatePickerStartDate(false);
-  };
-
-  const handleDateChangeEndDate = (selectedDate: Date) => {
-    // setData((prevData) => ({ ...prevData, date: selectedDate }));
-    setData((prevData) => ({ ...prevData, end_date: dayjs(selectedDate) }));
-    setOpenDatePickerEndDate(false);
-  };
-
-  const handleTimeChange = (selectedTime: Date) => {
-    // setData((prevData) => ({ ...prevData, time_check_in: selectedTime }));
-    setData((prevData) => ({ ...prevData, time_check_in: dayjs(selectedTime) }));
-    setOpenTimePicker(false);
-  };
+  }, [userDetailData, data.start_date, startDate, endDate, time, image]);
 
   const handleLocationChange = (text: string) => {
     const [latitude, longitude] = text.split(',').map(coord => parseFloat(coord.trim()));
@@ -92,37 +84,6 @@ function SakitScreen() {
       }));
     }
   };
-
-  const handleClickOpenCamera = async () => {
-    if (Platform.OS === 'android') {
-      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
-      if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
-        return Alert.alert('Permission Denied', 'Camera permission is required');
-      }
-    }
-
-    await launchCamera({
-      mediaType: 'photo',
-      quality: 0.5,
-      // includeBase64: true,
-      cameraType: 'front',
-    }, (response: any) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.errorCode) {
-        console.error('ImagePicker Error:', response.errorMessage);
-      } else {
-        setImage(response.assets[0]);
-        setData((prevData) => ({ ...prevData, image_check_in: response.assets[0] }));
-      }
-    });
-
-  }
-
-  const handleClickResetCamera = async () => {
-    setImage('');
-    setData((prevData) => ({ ...prevData, image_check_in: '' }));
-  }
 
   const handleSubmit = async () => {
 
@@ -190,129 +151,63 @@ function SakitScreen() {
     <SafeAreaView style={[styles.container]}>
       <ScrollView>
         <View style={[styles.formContainer]}>
-          <View style={[styles.groupField, { marginTop: 10 }]}>
-            <Text style={[styles.fieldLabel]}>Kode Absen</Text>
-            <TextInput
-              style={[styles.fieldInput]}
-              placeholder="Kode"
-              value={data.code}
-              onChangeText={(text) => setData((prevData) => ({ ...prevData, code: text }))}
-            />
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>NIK</Text>
-            <TextInput
-              style={[styles.fieldInput]}
-              placeholder="NIK"
-              value={data.nik}
-              onChangeText={(text) => setData((prevData) => ({ ...prevData, nik: text }))}
-            />
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>Nama</Text>
-            <TextInput
-              style={[styles.fieldInput]}
-              placeholder="Nama"
-              value={data.name}
-              onChangeText={(text) => setData((prevData) => ({ ...prevData, name: text }))}
-            />
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>Tanggal Awal Sakit</Text>
-            <View style={{ width: '100%', height: 45, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 15, paddingHorizontal: 10, justifyContent: 'center' }}>
-              <TextInput
-                style={{ color: '#242c40' }}
-                placeholder="Tanggal Awal Sakit"
-                value={data.start_date.format('DD/MM/YYYY')}
-                editable={false}
-              />
-              <TouchableOpacity style={{ position: 'absolute', right: 10 }} onPress={() => setOpenDatePickerStartDate(true)}>
-                <Icon name="calendar" size={20} color="#000" />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>Tanggal Akhir Sakit</Text>
-            <View style={{ width: '100%', height: 45, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 15, paddingHorizontal: 10, justifyContent: 'center' }}>
-              <TextInput
-                style={{ color: '#242c40' }}
-                placeholder="Tanggal Akhir Sakit"
-                value={data.end_date.format('DD/MM/YYYY')}
-                editable={false}
-              />
-              <TouchableOpacity style={{ position: 'absolute', right: 10 }} onPress={() => setOpenDatePickerEndDate(true)}>
-                <Icon name="calendar" size={20} color="#000" />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>Jam</Text>
-            <View style={{ width: '100%', height: 45, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 15, paddingHorizontal: 10, justifyContent: 'center' }}>
-              <TextInput
-                style={{ color: '#242c40' }}
-                placeholder="Jam"
-                value={data.time_check_in.format('HH:mm:ss')}
-                editable={false}
-              />
-              <TouchableOpacity style={{ position: 'absolute', right: 10 }} onPress={() => setOpenTimePicker(true)}>
-                <Icon name="clock" size={20} color="#000" />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>Bukti Sakit / Surat Dokter</Text>
-            {image ? (
-              <TouchableOpacity onPress={handleClickResetCamera} style={{ width: '100%', height: 45, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 15, paddingHorizontal: 10, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#242c40' }}>Reset Foto</Text>
-                <Icon name="trash-alt" size={20} color="#000" style={{ position: 'absolute', right: 10 }} />
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity onPress={handleClickOpenCamera} style={{ width: '100%', height: 45, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 15, paddingHorizontal: 10, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#242c40' }}>Ambil Foto</Text>
-                <Icon name="camera" size={20} color="#000" style={{ position: 'absolute', right: 10 }} />
-              </TouchableOpacity>
-
-            )}
-            {image && (
-              <View style={{ width: '100%', height: 200, marginBottom: 10, justifyContent: 'center', alignItems: 'center' }}>
-                <Image source={{ uri: image.uri }} style={{ width: '100%', height: '100%', resizeMode: 'contain' }} />
-              </View>
-            )}
-          </View>
-          <View style={[styles.groupField]}>
-            <Text style={[styles.fieldLabel]}>Lokasi Absen Sakit</Text>
-            <View style={{ width: '100%', height: 45, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 15, paddingHorizontal: 10, justifyContent: 'center' }}>
-              <TextInput
-                style={{ color: '#242c40' }}
-                placeholder="Lokasi Absen Sakit"
-                value={data.location_check_in}
-              // onChangeText={handleLocationChange}
-              />
-              <TouchableOpacity style={{ position: 'absolute', right: 10 }} onPress={getCurrentLocation}>
-                <Icon name="location-arrow" size={20} color="#000" />
-              </TouchableOpacity>
-            </View>
-            {data.latitude !== 0 && data.longitude !== 0 && (
-              <MapView
-                style={{ width: '100%', height: 200, marginBottom: 10, marginTop: -10 }}
-                initialRegion={{
-                  latitude: data.latitude,
-                  longitude: data.longitude,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}
-              >
-                <Marker
-                  coordinate={{
-                    latitude: data.latitude,
-                    longitude: data.longitude,
-                  }}
-                  title="Lokasi Anda"
-                  description={data.location_check_in}
-                />
-              </MapView>
-            )}
-          </View>
+          <InputField
+            label="Kode Absen"
+            placeholder="Kode"
+            value={data.code}
+            onChangeText={(text) => setData((prevData) => ({ ...prevData, code: text }))}
+          />
+          <InputField
+            label="NIK"
+            placeholder="NIK"
+            value={data.nik}
+            onChangeText={(text) => setData((prevData) => ({ ...prevData, nik: text }))}
+          />
+          <InputField
+            label="Nama"
+            placeholder="Nama"
+            value={data.name}
+            onChangeText={(text) => setData((prevData) => ({ ...prevData, name: text }))}
+          />
+          <InputField
+            label="Tanggal Awal Sakit"
+            placeholder="Tanggal Awal Sakit"
+            value={data.start_date.format('DD/MM/YYYY')}
+            onChangeText={() => {}}
+            editable={false}
+            onIconPress={() => setOpenDatePickerStartDate(true)}
+            iconName="calendar"
+          />
+          <InputField
+            label="Tanggal Akhir Sakit"
+            placeholder="Tanggal Akhir Sakit"
+            value={data.end_date.format('DD/MM/YYYY')}
+            onChangeText={() => {}}
+            editable={false}
+            onIconPress={() => setOpenDatePickerEndDate(true)}
+            iconName="calendar"
+          />
+          <InputField
+            label="Jam"
+            placeholder="Jam"
+            value={data.time_check_in.format('HH:mm:ss')}
+            onChangeText={() => {}}
+            editable={false}
+            onIconPress={() => setOpenTimePicker(true)}
+            iconName="clock"
+          />
+          <ImagePicker
+            label="Bukti Sakit / Surat Dokter"
+            image={image}
+            onOpenCamera={handleClickOpenCamera}
+            onResetCamera={handleClickResetCamera}
+          />
+          <LocationPicker
+            label="Lokasi Absen Sakit"
+            placeholder="Lokasi Absen Sakit"
+            location={location}
+            getCurrentLocation={getCurrentLocation}
+          />
           <View style={[styles.groupField, { marginBottom: 10 }]}>
             <TouchableOpacity style={{ backgroundColor: '#242c40', padding: 10, borderRadius: 5, alignItems: 'center' }} onPress={handleSubmit}>
               <Text style={{ color: 'white', fontWeight: 'bold' }}>Absen Sakit</Text>
@@ -366,19 +261,5 @@ const styles = StyleSheet.create({
   },
   groupField: {
     width: '100%',
-  },
-  fieldLabel: {
-    fontSize: 16,
-    marginBottom: 10,
-    fontWeight: 'bold',
-  },
-  fieldInput: {
-    width: '100%',
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    marginBottom: 15,
-    paddingHorizontal: 10,
   },
 });
