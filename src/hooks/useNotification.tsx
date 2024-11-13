f0eae177f6568201a0faa8e18514f4989af4ea27
dirhamtriyadi <dirhamtriyadi@gmail.com>
2024-11-13 10:05:57 +0700

    feat: improve notification functionality
    Adds a sound to the notifications and handles permission request exceptions.
    
    BREAKING CHANGE: The notification sound is now enabled by default.

--- src/hooks/useNotification.tsx
+++ src/hooks/useNotification.tsx
@@ -2,24 +2,27 @@ import notifee, { AndroidImportance } from '@notifee/react-native';
 
 export const useNotification = () => {
     const showNotification = async (title: string, body: string) => {
-        // Request permissions (required for iOS)
-        await notifee.requestPermission()
+        try {
+            // Request permissions (required for iOS)
+            await notifee.requestPermission()
+            // Create a channel (required for Android)
+            const channelId = await notifee.createChannel({
+                id: 'sound',
+                name: title,
+                sound: 'dutifully',
+                importance: AndroidImportance.HIGH,
+            });
 
-        // Create a channel (required for Android)
-        const channelId = await notifee.createChannel({
-            id: 'sound',
-            name: title,
-            sound: 'dutifully',
-            importance: AndroidImportance.HIGH,
-        });
-
-        await notifee.displayNotification({
-            title,
-            body,
-            android: {
-                channelId,
-            },
-        });
+            await notifee.displayNotification({
+                title,
+                body,
+                android: {
+                    channelId,
+                },
+            });
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return { showNotification };
