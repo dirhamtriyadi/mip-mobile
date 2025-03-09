7c52c3f1d6058640483942eef3b4c206bdbc970d
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-03-09 13:21:13 +0700

    feat(profil): integrate zod schema validation

--- src/screens/Profil/index.tsx
+++ src/screens/Profil/index.tsx
@@ -1,41 +1,59 @@
-import {ActivityIndicator, Alert, SafeAreaView, ScrollView} from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import {Text, View} from 'react-native';
 import styles from './styles';
 import globalStyles from '@styles/styles';
-import InputField from '@components/InputField';
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import instance from '../../configs/axios';
 import Button from '@src/components/Button';
-import {DataPassword, DataProfil} from '@src/types/profil';
+import {Controller, useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
 
 function ProfilScreen() {
   const [loading, setLoading] = useState<boolean>(false);
-  const [errors, setErrors] = useState<any>({});
 
-  const [formDataProfil, setFormDataProfil] = useState<DataProfil>({
-    _method: 'put',
-    name: '',
-    email: '',
-    nik: '',
+  const {
+    control: controlProfil,
+    handleSubmit: handleSubmitProfil,
+    setValue: setProfilValue,
+    formState: {errors: errorsProfil},
+  } = useForm<UpdateProfilSchema>({
+    resolver: zodResolver(updateProfilSchema),
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      email: '',
+      nik: '',
+    },
   });
 
-  const [formDataPassword, setFormDataPassword] = useState<DataPassword>({
-    _method: 'put',
-    password: '',
-    confirm_password: '',
+  const {
+    control: controlPassword,
+    handleSubmit: handleSubmitPassword,
+    setValue: setPasswordValue,
+    formState: {errors: errorsPassword},
+  } = useForm<UpdatePasswordSchema>({
+    resolver: zodResolver(updatePasswordSchema),
+    mode: 'onChange',
+    defaultValues: {
+      password: '',
+      confirm_password: '',
+    },
   });
 
   const getProfil = async () => {
     try {
       setLoading(true);
       const response = await instance.get('v1/profile');
-      setFormDataProfil(prevState => ({
-        ...prevState,
-        name: response.data.data.name,
-        email: response.data.data.email,
-        nik: response.data.data.detail_users?.nik,
-      }));
-      // console.log(response.data.data);
+      const {name, email, detail_users} = response.data.data;
+      setProfilValue('name', name);
+      setProfilValue('email', email);
+      setProfilValue('nik', detail_users?.nik || '');
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -46,29 +64,16 @@ function ProfilScreen() {
     getProfil();
   }, []);
 
-  const handleChangeProfil = (key: string, value: string) => {
-    setFormDataProfil(prevState => ({
-      ...prevState,
-      [key]: value,
-    }));
-  };
-
-  const handleChangePassword = (key: string, value: string) => {
-    setFormDataPassword(prevState => ({
-      ...prevState,
-      [key]: value,
-    }));
-  };
-
-  const handleSaveProfil = async () => {
+  const handleSaveProfil = async (data: UpdateProfilSchema) => {
     try {
       setLoading(true);
-
       instance.defaults.headers['Content-Type'] = 'application/json';
-      const response = await instance.put('v1/profile', formDataProfil);
+      const response = await instance.put('v1/profile', {
+        _method: 'put',
+        ...data,
+      });
       Alert.alert('Success', response.data.message);
     } catch (error: any) {
-      setErrors(error.response?.data?.errors || {});
       Alert.alert(
         'Error',
         error.response?.data?.message || 'Gagal menyimpan data',
@@ -78,24 +83,18 @@ function ProfilScreen() {
     }
   };
 
-  const handleSavePassword = async () => {
+  const handleSavePassword = async (data: UpdatePasswordSchema) => {
     try {
       setLoading(true);
-      if (formDataPassword.password !== formDataPassword.confirm_password) {
-        setErrors({confirm_password: ['Konfirmasi password tidak cocok']});
-        Alert.alert('Error', 'Konfirmasi password tidak cocok');
-        return;
-      }
-
       instance.defaults.headers['Content-Type'] = 'application/json';
-      const response = await instance.put(
-        'v1/profile/update-password',
-        formDataPassword,
-      );
+      const response = await instance.put('v1/profile/update-password', {
+        _method: 'put',
+        ...data,
+      });
       Alert.alert('Success', response.data.message);
-      setFormDataPassword({_method: 'put', password: '', confirm_password: ''});
+      setPasswordValue('password', '');
+      setPasswordValue('confirm_password', '');
     } catch (error: any) {
-      setErrors(error.response?.data?.errors || {});
       Alert.alert(
         'Error',
         error.response?.data?.message || 'Gagal mengupdate password',
@@ -125,41 +124,163 @@ function ProfilScreen() {
           <Text style={styles.title}>Edit Profil</Text>
         </View>
         <View style={[globalStyles.formContainer, styles.formGroup]}>
-          <InputField
-            label="Nama"
-            value={formDataProfil.name}
-            onChangeText={value => handleChangeProfil('name', value)}
-            placeholder="Masukan Nama Anda"
+          <Controller
+            control={controlProfil}
+            name="name"
+            render={({field: {onChange, onBlur, value}}) => (
+              <View style={{width: '100%'}}>
+                <Text
+                  style={[
+                    {fontSize: 16, marginBottom: 10, fontWeight: 'bold'},
+                    errorsProfil.name && {color: 'red'},
+                  ]}>
+                  Nama
+                </Text>
+                <TextInput
+                  placeholderTextColor={errorsProfil.name ? 'red' : 'black'}
+                  placeholder="Nama"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  style={[
+                    {
+                      color: 'black',
+                      width: '100%',
+                      height: 45,
+                      borderWidth: 1,
+                      borderColor: '#ccc',
+                      borderRadius: 5,
+                      marginBottom: 15,
+                      paddingHorizontal: 10,
+                      justifyContent: 'center',
+                      backgroundColor: '#fff',
+                    },
+                    errorsProfil.name && {borderColor: 'red', color: 'red'},
+                  ]}
+                  autoCorrect={false}
+                  value={value}
+                />
+              </View>
+            )}
           />
-          {errors && errors.name && (
-            <View style={styles.error}>
-              <Text style={styles.errorText}>{errors.name[0]}</Text>
-            </View>
+          {errorsProfil.name && (
+            <Text
+              style={{
+                color: 'red',
+                marginTop: -10,
+                marginBottom: 10,
+                width: '100%',
+              }}>
+              {errorsProfil.name.message}
+            </Text>
           )}
-          <InputField
-            label="Email"
-            value={formDataProfil.email}
-            onChangeText={value => handleChangeProfil('email', value)}
-            placeholder="Masukan Email Anda"
+
+          <Controller
+            control={controlProfil}
+            name="email"
+            render={({field: {onChange, onBlur, value}}) => (
+              <View style={{width: '100%'}}>
+                <Text
+                  style={[
+                    {fontSize: 16, marginBottom: 10, fontWeight: 'bold'},
+                    errorsProfil.email && {color: 'red'},
+                  ]}>
+                  Email
+                </Text>
+                <TextInput
+                  placeholderTextColor={errorsProfil.email ? 'red' : 'black'}
+                  placeholder="Email"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  style={[
+                    {
+                      color: 'black',
+                      width: '100%',
+                      height: 45,
+                      borderWidth: 1,
+                      borderColor: '#ccc',
+                      borderRadius: 5,
+                      marginBottom: 15,
+                      paddingHorizontal: 10,
+                      justifyContent: 'center',
+                      backgroundColor: '#fff',
+                    },
+                    errorsProfil.email && {borderColor: 'red', color: 'red'},
+                  ]}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  value={value}
+                />
+              </View>
+            )}
           />
-          {errors && errors.email && (
-            <View style={styles.error}>
-              <Text style={styles.errorText}>{errors.email[0]}</Text>
-            </View>
+          {errorsProfil.email && (
+            <Text
+              style={{
+                color: 'red',
+                marginTop: -10,
+                marginBottom: 10,
+                width: '100%',
+              }}>
+              {errorsProfil.email.message}
+            </Text>
           )}
-          <InputField
-            editable={false}
-            label="NIK"
-            value={formDataProfil.nik}
-            onChangeText={value => handleChangeProfil('nik', value)}
-            placeholder="Masukan NIK Anda"
+
+          <Controller
+            control={controlProfil}
+            name="nik"
+            render={({field: {onChange, onBlur, value}}) => (
+              <View style={{width: '100%'}}>
+                <Text
+                  style={[
+                    {fontSize: 16, marginBottom: 10, fontWeight: 'bold'},
+                    errorsProfil.nik && {color: 'red'},
+                  ]}>
+                  NIK
+                </Text>
+                <TextInput
+                  placeholderTextColor={errorsProfil.nik ? 'red' : 'black'}
+                  placeholder="NIK"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  style={[
+                    {
+                      color: 'black',
+                      width: '100%',
+                      height: 45,
+                      borderWidth: 1,
+                      borderColor: '#ccc',
+                      borderRadius: 5,
+                      marginBottom: 15,
+                      paddingHorizontal: 10,
+                      justifyContent: 'center',
+                      backgroundColor: '#fff',
+                    },
+                    errorsProfil.nik && {borderColor: 'red', color: 'red'},
+                  ]}
+                  keyboardType="numeric"
+                  autoCorrect={false}
+                  value={value}
+                />
+              </View>
+            )}
           />
-          {errors && errors.nik && (
-            <View style={styles.error}>
-              <Text style={styles.errorText}>{errors.nik[0]}</Text>
-            </View>
+          {errorsProfil.nik && (
+            <Text
+              style={{
+                color: 'red',
+                marginTop: -10,
+                marginBottom: 10,
+                width: '100%',
+              }}>
+              {errorsProfil.nik.message}
+            </Text>
           )}
-          <Button label="Simpan" onPress={() => handleSaveProfil()} />
+
+          <Button
+            label="Simpan"
+            onPress={handleSubmitProfil(handleSaveProfil)}
+          />
         </View>
         <View
           style={[
@@ -170,35 +291,128 @@ function ProfilScreen() {
           <Text style={styles.title}>Edit Password</Text>
         </View>
         <View style={[globalStyles.formContainer, styles.formGroup]}>
-          <InputField
-            label="Password"
-            value={formDataPassword.password}
-            onChangeText={value => handleChangePassword('password', value)}
-            placeholder="Masukan Password Baru"
+          <Controller
+            control={controlPassword}
+            name="password"
+            render={({field: {onChange, onBlur, value}}) => (
+              <View style={{width: '100%'}}>
+                <Text
+                  style={[
+                    {fontSize: 16, marginBottom: 10, fontWeight: 'bold'},
+                    errorsPassword.password && {color: 'red'},
+                  ]}>
+                  Password
+                </Text>
+                <TextInput
+                  placeholderTextColor={
+                    errorsPassword.password ? 'red' : 'black'
+                  }
+                  placeholder="Password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  style={[
+                    {
+                      color: 'black',
+                      width: '100%',
+                      height: 45,
+                      borderWidth: 1,
+                      borderColor: '#ccc',
+                      borderRadius: 5,
+                      marginBottom: 15,
+                      paddingHorizontal: 10,
+                      justifyContent: 'center',
+                      backgroundColor: '#fff',
+                    },
+                    errorsPassword.password && {
+                      borderColor: 'red',
+                      color: 'red',
+                    },
+                  ]}
+                  value={value}
+                />
+              </View>
+            )}
           />
-          {errors && errors.password && (
-            <View style={styles.error}>
-              <Text style={styles.errorText}>{errors.password[0]}</Text>
-            </View>
+          {errorsPassword.password && (
+            <Text
+              style={{
+                color: 'red',
+                marginTop: -10,
+                marginBottom: 10,
+                width: '100%',
+              }}>
+              {errorsPassword.password.message}
+            </Text>
           )}
-          <InputField
-            label="Konfirmasi Password"
-            value={formDataPassword.confirm_password}
-            onChangeText={value =>
-              handleChangePassword('confirm_password', value)
-            }
-            placeholder="Masukan Konfirmasi Password"
+
+          <Controller
+            control={controlPassword}
+            name="confirm_password"
+            render={({field: {onChange, onBlur, value}}) => (
+              <View style={{width: '100%'}}>
+                <Text
+                  style={[
+                    {fontSize: 16, marginBottom: 10, fontWeight: 'bold'},
+                    errorsPassword.confirm_password && {color: 'red'},
+                  ]}>
+                  Konfirmasi Password
+                </Text>
+                <TextInput
+                  placeholderTextColor={
+                    errorsPassword.confirm_password ? 'red' : 'black'
+                  }
+                  placeholder="Konfirmasi Password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  style={[
+                    {
+                      color: 'black',
+                      width: '100%',
+                      height: 45,
+                      borderWidth: 1,
+                      borderColor: '#ccc',
+                      borderRadius: 5,
+                      marginBottom: 15,
+                      paddingHorizontal: 10,
+                      justifyContent: 'center',
+                      backgroundColor: '#fff',
+                    },
+                    errorsPassword.confirm_password && {
+                      borderColor: 'red',
+                      color: 'red',
+                    },
+                  ]}
+                  value={value}
+                />
+              </View>
+            )}
           />
-          {errors && errors.confirm_password && (
-            <View style={styles.error}>
-              <Text style={styles.errorText}>{errors.confirm_password[0]}</Text>
-            </View>
+          {errorsPassword.confirm_password && (
+            <Text
+              style={{
+                color: 'red',
+                marginTop: -10,
+                marginBottom: 10,
+                width: '100%',
+              }}>
+              {errorsPassword.confirm_password.message}
+            </Text>
           )}
-          <Button label="Simpan" onPress={() => handleSavePassword()} />
+
+          <Button
+            label="Simpan"
+            onPress={handleSubmitPassword(handleSavePassword)}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+import {
+  updatePasswordSchema,
+  UpdatePasswordSchema,
+  updateProfilSchema,
+  UpdateProfilSchema,
+} from '@src/schema/profil';
 
 export default ProfilScreen;
