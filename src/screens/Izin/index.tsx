ce3f207613ff3a2ab7c174eeea5bd0d8c70f1ab4
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-03-09 14:13:08 +0700

    feat(izin): pre-fill izin form with user data

--- src/screens/Izin/index.tsx
+++ src/screens/Izin/index.tsx
@@ -19,6 +19,16 @@ import globalStyles from '@styles/styles';
 import Button from '@src/components/Button';
 
 function IzinScreen() {
+  const {image, handleClickOpenCamera, handleClickReset} = useImagePicker();
+  const {date, openDatePicker, setOpenDatePicker, handleDateChange} =
+    useDatePicker(dayjs());
+  const {time, openTimePicker, setOpenTimePicker, handleTimeChange} =
+    useTimePicker(dayjs());
+  const {userDetailData} = useUserData();
+  const {location, getCurrentLocation} = useLocation();
+  const {showNotification} = useNotification();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
   const [data, setData] = useState({
     code: '',
     nik: '',
@@ -32,17 +42,25 @@ function IzinScreen() {
     longitude: 0,
   });
 
-  const {image, handleClickOpenCamera, handleImageSelect, handleClickReset} =
-    useImagePicker();
-  const {date, openDatePicker, setOpenDatePicker, handleDateChange} =
-    useDatePicker(data.date);
-  const {time, openTimePicker, setOpenTimePicker, handleTimeChange} =
-    useTimePicker(data.time_check_in);
-  const {userDetailData} = useUserData();
-  const {location, getCurrentLocation} = useLocation();
-  const {showNotification} = useNotification();
+  useEffect(() => {
+    if (userDetailData.nik) {
+      setData(prevData => ({
+        ...prevData,
+        code: `${userDetailData.name}_${dayjs().format('DDMMYYYY')}`,
+        nik: userDetailData.nik,
+        name: userDetailData.name,
+      }));
+    }
+  }, [userDetailData]);
 
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  useEffect(() => {
+    setData(prevData => ({
+      ...prevData,
+      date,
+      time_check_in: time,
+      image_check_in: image?.uri || '',
+    }));
+  }, [date, time, image]);
 
   useEffect(() => {
     if (location.latitude !== 0 && location.longitude !== 0) {
@@ -55,97 +73,38 @@ function IzinScreen() {
     }
   }, [location]);
 
-  useEffect(() => {
-    setData(prevData => ({
-      ...prevData,
-      code: userDetailData.name + data.date.format('DD/MM/YYYY'),
-      nik: userDetailData.nik,
-      name: userDetailData.name,
-      date: date,
-      time_check_in: time,
-      image_check_in: image,
-    }));
-  }, [userDetailData, data.date, date, time, image]);
-
-  const handleLocationChange = (text: string) => {
-    const [latitude, longitude] = text
-      .split(',')
-      .map(coord => parseFloat(coord.trim()));
-    if (!isNaN(latitude) && !isNaN(longitude)) {
-      setData(prevData => ({
-        ...prevData,
-        location: text,
-        latitude,
-        longitude,
-      }));
-    } else {
-      setData(prevData => ({
-        ...prevData,
-        location: text,
-      }));
-    }
-  };
-
   const handleSubmit = async () => {
-    // add validation here
-    if (data.code === '') {
-      return Alert.alert('Kode absen harus diisi');
-    }
-    if (data.nik === '') {
-      return Alert.alert('NIK harus diisi');
-    }
-    if (data.name === '') {
-      return Alert.alert('Nama harus diisi');
-    }
-    // if (data.image_check_in === '') {
-    //   return Alert.alert('Foto izin harus diisi');
-    // }
-    if (data.location_check_in === '') {
-      return Alert.alert('Lokasi harus diisi');
+    if (!data.code || !data.nik || !data.name || !data.location_check_in) {
+      return Alert.alert('Error', 'Semua data harus diisi.');
     }
 
     try {
-      const {date, time_check_in, type, location_check_in} = data;
-
       const formData = new FormData();
+      formData.append('date', data.date.format('YYYY-MM-DD'));
+      formData.append('time_check_in', data.time_check_in.format('HH:mm:ss'));
+      formData.append('type', data.type);
+      formData.append('location_check_in', data.location_check_in);
 
-      formData.append('date', date.format('YYYY-MM-DD'));
-      formData.append('time_check_in', time_check_in.format('HH:mm:ss'));
-      formData.append('type', type);
-      formData.append('location_check_in', location_check_in);
-
-      // Add image file to formData
       if (image) {
         formData.append('image_check_in', {
           uri: image.uri,
-          type: image.type,
-          name: image.fileName,
+          type: image.type || 'image/jpeg',
+          name: image.fileName || `izin_${Date.now()}.jpg`,
         });
       }
 
       instance.defaults.headers['Content-Type'] = 'multipart/form-data';
 
       await instance.post('v1/attendances/permit', formData);
-      Alert.alert('Absen izin berhasil', 'Absen izin berhasil disubmit', [
-        {
-          text: 'OK',
-          onPress: () => navigation.navigate('Home'),
-        },
+      Alert.alert('Sukses', 'Absen izin berhasil disubmit', [
+        {text: 'OK', onPress: () => navigation.navigate('Home')},
       ]);
-      showNotification('Absen izin berhasil', 'Absen izin berhasil disubmit');
+      showNotification('Sukses', 'Absen izin berhasil disubmit');
     } catch (error: any) {
-      if (error.response?.data?.message?.code) {
-        error.response?.data?.message?.code.map((item: any) => {
-          // console.log(item);
-          return Alert.alert('Absen Izin Gagal', item);
-        });
-      } else {
-        Alert.alert(
-          'Absen Izin Gagal',
-          'Gagal terjadi kesalahan karena:\n' + error.response.data.message,
-        );
-        // console.log('Error submitting absen izin: ', error.response.data.message);
-      }
+      const errorMessage =
+        error.response?.data?.message ||
+        'Terjadi kesalahan saat mengirim data.';
+      Alert.alert('Gagal', errorMessage);
     }
   };
 
@@ -153,44 +112,19 @@ function IzinScreen() {
     <SafeAreaView style={globalStyles.container}>
       <ScrollView>
         <View style={globalStyles.formContainer}>
-          <InputField
-            label="Kode Absen"
-            placeholder="Kode"
-            value={data.code}
-            onChangeText={text =>
-              setData(prevData => ({...prevData, code: text}))
-            }
-          />
-          <InputField
-            label="NIK"
-            placeholder="NIK"
-            value={data.nik}
-            onChangeText={text =>
-              setData(prevData => ({...prevData, nik: text}))
-            }
-          />
-          <InputField
-            label="Nama"
-            placeholder="Nama"
-            value={data.name}
-            onChangeText={text =>
-              setData(prevData => ({...prevData, name: text}))
-            }
-          />
+          <InputField label="Kode Absen" value={data.code} editable={false} />
+          <InputField label="NIK" value={data.nik} editable={false} />
+          <InputField label="Nama" value={data.name} editable={false} />
           <InputField
             label="Tanggal"
-            placeholder="Tanggal"
             value={data.date.format('DD/MM/YYYY')}
-            onChangeText={() => {}}
             editable={false}
             onIconPress={() => setOpenDatePicker(true)}
             iconName="calendar"
           />
           <InputField
             label="Jam"
-            placeholder="Jam"
             value={data.time_check_in.format('HH:mm:ss')}
-            onChangeText={() => {}}
             editable={false}
             onIconPress={() => setOpenTimePicker(true)}
             iconName="clock"
@@ -203,19 +137,15 @@ function IzinScreen() {
           />
           <LocationPicker
             label="Lokasi Absen Izin"
-            placeholder="Lokasi Absen Izin"
             location={location}
             getCurrentLocation={getCurrentLocation}
           />
-          <View style={[globalStyles.groupField, {marginBottom: 10}]}>
-            <Button label="Simpan" onPress={handleSubmit} />
-          </View>
+          <Button label="Simpan" onPress={handleSubmit} />
         </View>
       </ScrollView>
       <DatePicker
         modal
         mode="date"
-        minimumDate={dayjs().hour(0).minute(0).second(0).toDate()}
         open={openDatePicker}
         date={data.date.toDate()}
         onConfirm={handleDateChange}
@@ -224,7 +154,6 @@ function IzinScreen() {
       <DatePicker
         modal
         mode="time"
-        minimumDate={dayjs().toDate()}
         open={openTimePicker}
         date={data.time_check_in.toDate()}
         onConfirm={handleTimeChange}
